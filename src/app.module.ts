cb820ee0ca69c8d859894987d93996f262c14a9d
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2022-11-09 20:39:09 +0700

    config port

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,7 +48,7 @@ import { CurrencyLog } from './modules/currency-log/entities/currency-log.entity
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      // port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
