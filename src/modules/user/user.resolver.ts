32afb118d08e71d8959636f877a754f32bc50bec
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2022-11-11 15:03:19 +0700

    get user query

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -3,9 +3,32 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  @Query(() => User)
+  async getUserByEmail(
+    @Args("Email") Email: string 
+  ) : Promise<User> {
+    try {
+      return await this.userService.getUserByEmail(Email);
+    } catch(error) {
+      throw new HttpException(error.message, error.status || HttpStatus.BAD_REQUEST );
+    }
+  }
+
+  
+  @Query(() => User)
+  async getUserById(
+    @Args("User_ID") User_ID: string 
+  ) : Promise<User> {
+    try {
+      return await this.userService.getUserById(User_ID);
+    } catch(error) {
+      throw new HttpException(error.message, error.status || HttpStatus.BAD_REQUEST );
+    }
+  }
 }
