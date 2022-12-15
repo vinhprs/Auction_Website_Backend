b31e9f119f2123136c9c043accbff342bf9a6c89
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2022-12-15 18:30:31 +0700

    remove currency guards

--- src/modules/currency/currency.resolver.ts
+++ src/modules/currency/currency.resolver.ts
@@ -25,7 +25,6 @@ export class CurrencyResolver {
   }
 
   @Query(() => Currency)
-  @UseGuards(JwtAuthGuard)
   async getCurrentByUser(
     @Args('User_ID') User_ID: string
   ) : Promise<Currency> {
